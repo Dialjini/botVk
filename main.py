cc49e6a947c516a787e432e0b97d97b087fdf0e7
Dialjini <danil_kustov@mail.ru>
2019-04-30 22:40:40 +0700

    add ssl support

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ from threading import Thread
 import dbase
 from cherrypy.lib import static
 
+
+url = 'test.vkApp.com'
+
 botactive = False
 url = 'https://api.vk.com/method/users.get?user_id=294940138&v=5.52&access_token=737c6a19d16db39e7dee92e584c64717b125dbc158aebb486c5cec455570a515473c1e3d265c970b1255c'
+secKey = 'D7UqxfSz3SU8bW5fJmnF'
+serviceKey = '88359d2a88359d2a88359d2a5f885c21918883588359d2ad492dab541f7422bd024a392'
 
-print(requests.post(url=url).text)
+# print(requests.post(url=url).text)
 
 @cherrypy.expose
 class GetForm(object):
@@ -62,11 +67,14 @@ conf = {
     }
 
 cherrypy.config.update({'server.socket_host': '127.0.0.1',
-                            'server.socket_port': 8080,
+                            'server.socket_port': 443,
                             'tools.sessions.on': True,
                             'engine.autoreload.on': False,
                             'log.access_file': './access.log',
                             'log.error_file': './error.log',
+                            'server.ssl_module': 'builtin',
+                            'server.ssl_certificate': 'cert.pem',
+                            'server.ssl_private_key': 'privkey.pem'
                             })
 
 cherrypy.tree.mount(GenerateHtml(), '/', conf)
