47920ab850d02a68a23c466e2815d2bda97f8b0e
Daniil Soldatov <of.swg.csgo@gmail.com>
2019-05-03 09:58:26 +0700

    Some changes

--- public/js/script.js
+++ public/js/script.js
@@ -10,70 +10,79 @@ var apikey = '4t1278fbskd'
 
 
 function getWidget() {
-    $.ajax({
-        type: "GET",
-        url: "/getWidget",
+  $.ajax({
+      type: "GET",
+      url: "/getWidget",
     })
     .done(function(result) {
-        alert(result);
+      alert(result);
     });
 
 }
 
 function main() {
-    $.ajax({
-        type: "GET",
-        url: "/getForm",
-        data: {'botname': botname, 'login': login}
+  $.ajax({
+      type: "GET",
+      url: "/getForm",
+      data: {
+        'botname': botname,
+        'login': login
+      }
     })
     .done(function(result) {
-        console.log(result);
-        console.log(JSON.parse(result));
-        result = JSON.parse(result);
-        date0 = result['date'];
-        count = parseInt(result['balance']);
-        time = result['count'];
-        document.getElementById("date").innerHTML = date0;
-        document.getElementById("count").innerHTML = count.toFixed(2) + " &#8381;";
-        document.getElementById("countTo").innerHTML = count.toFixed(2) + " &#8381;";
-        document.getElementById("tariff").innerHTML = tariff;
-        document.getElementById("time").innerHTML = time;
+      console.log(result);
+      console.log(JSON.parse(result));
+      result = JSON.parse(result);
+      date0 = result['date'];
+      count = parseInt(result['balance']);
+      time = result['count'];
+      id('date').innerHTML = date0;
+      id('count').innerHTML = count.toFixed(2) + " &#8381;";
+      id('countTo').innerHTML = count.toFixed(2) + " &#8381;";
+      id('tariff').innerHTML = tariff;
+      id('time').innerHTML = time;
 
     });
 
   //document.getElementById("count").innerHTML = count.toFixed(2) + " &#8381;";
- // document.getElementById("countTo").innerHTML = count.toFixed(2) + " &#8381;";
+  // document.getElementById("countTo").innerHTML = count.toFixed(2) + " &#8381;";
   //document.getElementById("tariff").innerHTML = tariff;
- // document.getElementById("time").innerHTML = time;
+  // document.getElementById("time").innerHTML = time;
 
 }
 
 function checkBot(checkbox) {
-     if(document.getElementById("count").innerHTML) {
-        if (checkbox.checked) {
-            $.ajax({
-                type: "GET",
-                url: "/botStatus",
-                data: {'botname': botname, 'login': login, 'mode': 'on'}
-            })
-            .done(function() {
-                alert("Бот включен");
-            });
-            }
-            else {
-                $.ajax({
-                    type: "GET",
-                    url: "/botStatus",
-                    data: {'botname': botname, 'login': login, 'mode': 'off'}
-                })
-                .done(function() {
-                    alert("Бот выключен");
-                });
-            }
+  if (id('count').innerHTML) {
+    if (checkbox.checked) {
+      $.ajax({
+          type: "GET",
+          url: "/botStatus",
+          data: {
+            'botname': botname,
+            'login': login,
+            'mode': 'on'
+          }
+        })
+        .done(function() {
+          alert("Бот включен");
+        });
+    } else {
+      $.ajax({
+          type: "GET",
+          url: "/botStatus",
+          data: {
+            'botname': botname,
+            'login': login,
+            'mode': 'off'
+          }
+        })
+        .done(function() {
+          alert("Бот выключен");
+        });
     }
-    else {
-        alert('Сначала введите свои данные, после чего нажмите кнопку "Сохранить"');
-    }
+  } else {
+    alert('Сначала введите свои данные, после чего нажмите кнопку "Сохранить"');
+  }
 }
 
 function settings() {
@@ -90,27 +99,32 @@ function card() {
   $('.card').css('display', 'block');
 }
 
-function saveSettings(){
+function saveSettings() {
 
-    var eMail = id('email').value;
-    var lkcrm = id('linkCRM').value;
-    var apikey = id('api-key').value;
+  var eMail = id('email').value;
+  var lkcrm = id('linkCRM').value;
+  var apikey = id('api-key').value;
+
+  console.log(crm);
+
+  if (eMail != "") {
+    crm = eMail;
+  }
 
 
-    console.log(crm);
-
-    if (eMail != "") {
-      crm = eMail;
-    }
-
-
-    $.ajax({
-        type: "GET",
-        url: "/updateClient",
-        data: {'botname': botname, 'login': login, 'crm': crm, 'lkcrm': lkcrm, 'apikey': apikey}
+  $.ajax({
+      type: "GET",
+      url: "/updateClient",
+      data: {
+        'botname': botname,
+        'login': login,
+        'crm': crm,
+        'lkcrm': lkcrm,
+        'apikey': apikey
+      }
     })
     .done(function() {
-        console.log('settings saved');
+      console.log('settings saved');
     });
 }
 
@@ -121,7 +135,7 @@ function selectMode(selectObject) {
   } else {
     $('.email').fadeOut(100);
   }
- }
+}
 
 function id(id) {
   return document.getElementById(id);
